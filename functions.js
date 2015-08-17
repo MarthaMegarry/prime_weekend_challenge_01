f6ec6cc5a805704552ccaf123f8b8c224d6930f4
MarthaMegarry <mega0015@umn.edu>
2015-08-17 11:24:25 -0500

    After the in-classroom demo of how to solve it I went back using the new code to update my solution so it has full functionality

--- functions.js
+++ functions.js
@@ -1,116 +1,58 @@
 $(document).ready(function(){
-	var newEmployee = {};
-	var empArray = [];
-	var newEl, newText, position, newButton, newButtonText, position2;
+	var $form = $('#empForm');
+	var $firstname = $('#firstname');
+	var $lastname = $('#lastname');
+	var $employeenumber = $('#employeenumber');
+	var $emptitle = $('#emptitle');
+	var $reviewscore = $('#reviewscore');
+	var $salary = $('#salary');
+	var $list = $('#list');
 
-	position = document.getElementById('list');
+	//var newEl, newText, position, newButton, newButtonText, position2;
 
-	console.log(newEmployee);
+	//position = document.getElementById('list');
+
+	//console.log(newEmployee);
 	
-	$('#submit').on('click', function(e){
-		var $inputs = $('#empForm :input');
-		console.log($inputs);
-		//var $inputEmpform = $('#empForm');
-		//newEmployee[$inputEmpform.name] = $inputEmpform.val();
-
-		$inputs.each(function(){
-			newEmployee[this.name] = $(this).val();
-		});
-
-		var myNewElement = $('<li>First name: ' + newEmployee.firstname +  ' | Last Name: ' + newEmployee.lastname + ' | Employee Number: ' + newEmployee.employeenumber + ' | Employee Title: ' + newEmployee.emptitle + ' | Review Score: ' + newEmployee.reviewscore + ' | Salary: ' + newEmployee.salary + '  <div class="button">Remove</div></li>');
-
-		myNewElement.appendTo( "#list" );
-
-		//var myNewButton = $('<div> Remove </div>');
-		//myNewButton.appendTo( ".shown" );	
-		//$('div').attr('class', 'button');		
-		
+	$form.on('submit', function(e){
 		e.preventDefault();
-
-		$('div').on('click', function(){
-			$(this).parent('li').remove();
-		});
-
-		console.log(newEmployee);
-
-		//	if(newEmployee.reviewscore < 2){
-		//	$(myNewElement).attr("class", "low");
-		//} else if(newEmployee.reviewscore = 3){
-		//	$(myNewElement).attr("class", "middle");
-		//} else {
-		//	$(myNewElement).attr("class", "high");
-		//}
-
-		switch(newEmployee.reviewscore) {
-			case 1:
-				$('myNewElement').attr("class", "low");
-				break;
-			case 2:
-				$('myNewElement').attr("class", "low");
-				break;
-			case 3:
-				$('myNewElement').attr("class", "middle");
-				break;
-			case 4:
-				$('myNewElement').attr("class", "high");
-				break;
-			case 5:
-				$('myNewElement').attr("class", "high");
-				break;	
-		}
+		var newEmployee = new Employee(
+			$firstname.val(),
+			$lastname.val(),
+			$employeenumber.val(),
+			$emptitle.val(),
+			$reviewscore.val(),
+			$salary.val()	
+		);
+		appendEmployee(newEmployee);
 	});
+
+	var Employee = function(firstName, lastName, empNum, empTitle, score, salary) {
+    	this.firstname = firstName;
+    	this.lastname = lastName;
+    	this.employeenumber = empNum;
+    	this.emptitle = empTitle;
+    	this.reviewscore = score;
+    	this.salary = salary;
+  	}	
+
+  	function appendEmployee(emp){
+    	var $li = $('<li>');
+    	$li.attr('class', 'score' + emp.reviewscore);
+    	$li.text('First name: ' + emp.firstname +  ' | Last Name: ' + emp.lastname + ' | Employee Number: ' + emp.employeenumber + ' | Employee Title: ' + emp.emptitle + ' | Review Score: ' + emp.reviewscore + ' | Salary: ' + emp.salary
+    	);
+
+    	var $button = $('<button>');
+    	$button.text('Delete');
+    	$button.attr('class', 'js-delete');
+
+    	$li.append($button);
+
+    	$list.append($li);
+  	}
+
+	$list.on('click', '.js-delete', function(e){
+    	e.preventDefault();
+    	$(this).parent().remove();
+  	})
 });
-
-
-
-
-		//newEl = document.createElement('li');
-		//newText = document.createTextNode('First name: ' + newEmployee.firstname +  ' | Last Name: ' + newEmployee.lastname + ' | Employee Number ' + newEmployee.employeenumber + ' | Employee Title: ' + newEmployee.emptitle + ' | Review Score: ' + newEmployee.reviewscore + ' | Salary: ' + newEmployee.salary);
-		//newButton = document.createElement('div');
-		//newButtonText = document.createTextNode("Remove")
-
-		//newEl.appendChild(newText);
-		//position.appendChild(newEl);
-		//$('li').attr('id', 'shown');
-
-		//position2 = document.getElementById('shown');
-		//newButton.appendChild(newButtonText);
-		//position2.appendChild(newButton);
-		//$('div').attr('class', 'button');
-		
-
-
-
-		//newEmployee.firstname = $('#firstname').val();
-		//newEmployee.lastname = $('#lastname').val();
-		//newEmployee.employeenumber = $('#employeenumber').val();
-		//newEmployee.emptitle = $('#emptitle').val();
-		//newEmployee.reviewscore = $('#reviewscore').val();
-		//newEmployee.salary = $('#salary').val();
-
-		//console.log(newEmployee);
-		
-		//employeeArray.push(newEmployee);
-		
-		//console.log(employeeArray);
-
-		//for(i = 0; i < employeeArray.length; i++) {
-		//	$('ul').append('<li> First name: ' + employeeArray[i].firstname + '</li>');
-		//}
-
-		//e.preventDefault();
-	//});
-
-
-//$('#submit').on('click', function(){
-		//var $inputs = $('#empForm :input');
-		//$inputs.each(function(){
-			//values[this.name] = $(this).var();
-		//});
-//});
-
-//$( "#empForm" ).submit(function( event ) {
-  //		alert( "Handler for .submit() called." );
-  	//	event.preventDefault();
-  		
-//	});
